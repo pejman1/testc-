1820faf1504a61a05d2695a045a2da08f4b82761
Pejman <pejman@pejmans-iMac.local>
2015-11-13 19:58:40 +0330

    Initial Commit

---
+++ testc++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testc++
+//
+//  Created by Pejman on 11/13/15.
+//  Copyright (c) 2015 pejman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

b53302fe77226874b884cfb8260555278ee42fec
Pejman <pejman.p@me.com>
2016-03-13 14:08:37 +0330

    test github

---
+++ File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  testc++
+//
+//  Created by Pejman on 11/13/15.
+//  Copyright (c) 2015 pejman. All rights reserved.
+//
+
+import Foundation
+
+let c= 50;
+println(c);
--- testc++/main.cpp
+++ testc++/main.cpp
@@ -11,5 +11,13 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    for (int i=0 ; i<100 ; i++) {
+
+        
+        
+        std::cout << i ;
+        std::cout << "\n ";
+    }
+    
     return 0;
 }
